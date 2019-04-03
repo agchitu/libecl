equinor/resdata

Read nexus data
Contents of a nexus plot file are read and stored in a sequential data
structure.

lib/nexus/nexus_plot.cpp
@@ -18,9 +18,12 @@
 
 #include <algorithm>
 #include <array>
-#include <sstream>
+#include <cstring>
 #include <iostream>
+#include <map>
 #include <sstream>
+#include <set>
+
 
 #include <ert/util/build_config.h>
 #ifdef HAVE_NETINET_IN_H
@@ -32,128 +35,203 @@
 #endif
 
 #include <ert/nexus/nexus_plot.hpp>
-#include <ert/ecl/ecl_sum.h>
 
+using namespace nex;
 
-const std::string NEXUS_PLOT_TYPE_HEADER = "PLOT  BIN ";
 
 namespace {
 
-template< int N >
-std::string read_str(std::istream& stream) {
-    std::array< char, N> buf;
-    stream.read( buf.data(), N );
-    return std::string( buf.data(), N );
+uint32_t ntoh(uint32_t ui) { return ntohl(ui); }
+
+float interpret_float(int32_t i) {
+    float f;
+    std::memcpy( &f, &i, sizeof( float ) );
+    return f;
 }
 
-struct hinfo {
-    int32_t num_classes;
-    int32_t day, month, year;
-    int32_t nx, ny, nz;
-    int32_t ncomp;
-    std::vector<std::string> class_names;
-    std::vector<int32_t> vars_in_class;
-    std::vector< std::vector<std::string> > var_names;
-};
+template< int N >
+using str = std::array< char, N >;
 
-hinfo headerinfo( std::istream& stream ) {
-    stream.seekg(4 + 10 + 562 + 264, std::ios::beg);
+template< int N >
+str< N > read_str(std::istream& stream) {
+    std::array< char, N> buf;
+    stream.read( buf.data(), N );
+    return buf;
+}
+
+NexusHeader read_header( std::istream& stream ) {
+    stream.seekg(4, std::ios::beg); // skip 4 bytes
+
+    static constexpr const char th0[11] = "PLOT  BIN ";
+    auto th1 = read_str<10>(stream);
+    auto ok = std::equal(std::begin(th1), std::end(th1), std::begin(th0));
+
+    if (!ok || !stream.good())
+        throw bad_header("Could not verify file type");
+
+    // skip unknown blob
+    stream.seekg(562 + 264, std::ios::cur);
 
     std::array< int32_t, 8 > buf {};
     stream.read( (char*)buf.data(), buf.max_size() * 4 );
-    if ( !stream.good() ) throw nex::unexpected_eof("");
+    if ( !stream.good() ) throw unexpected_eof("");
 
-    auto ntoh = []( int32_t x ) { return ntohl( x ); };
     std::transform( buf.begin(), buf.end(), buf.begin(), ntoh );
     auto negative = []( int32_t x ) { return x < 0; };
     if ( std::any_of( buf.begin(), buf.end(), negative ) )
-        throw nex::bad_header("Negative value, corrupted file");
+        throw bad_header("Negative value, corrupted file");
 
-    hinfo h = {
+    NexusHeader h = {
         buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7],
-        {},
-        std::vector< int32_t >( buf[0], 0 ),
-        std::vector< std::vector<std::string> >( buf[0],
-            std::vector<std::string>()),
     };
-
-    stream.seekg(8, std::ios::cur);
-    std::array< char, 8 > class_name;
-    for (int i = 0; i < h.num_classes; i++) {
-        stream.read(class_name.data(), 8);
-        h.class_names.push_back(std::string( class_name.data(), 8 ));
-    }
-
-    stream.seekg(8, std::ios::cur);
-    stream.read((char*) h.vars_in_class.data(), h.num_classes * 4);
-    std::transform( h.vars_in_class.begin(),
-                    h.vars_in_class.end(),
-                    h.vars_in_class.begin(),
-                    ntoh );
-    if (std::any_of( h.vars_in_class.begin(), h.vars_in_class.end(), negative))
-        throw nex::bad_header("Negative value, corrupted file");
-
-    stream.seekg(8, std::ios::cur);
-    for (int i = 0; i < h.num_classes; ++i) {
-        stream.seekg(4, std::ios::cur);
-        std::vector< char > var_names( h.vars_in_class[i] * 4, 0 );
-        stream.read( var_names.data(), h.vars_in_class[i] * 4 );
-        for (int k = 0; k < h.vars_in_class[i]; ++k)
-            h.var_names[i].push_back( std::string( var_names.data() + k*4 ,4 ));
-        stream.seekg(8, std::ios::cur);
-    }
-    stream.seekg(4, std::ios::cur);
 
     return h;
 }
 
+std::map< str<8>, std::vector<str<4>> >
+read_varnames( std::istream& stream, int32_t num_classes )  {
+    std::vector< str<8> > classnames;
+    std::vector< int32_t > vars_in_class( num_classes );
+    std::map< str<8>, std::vector<str<4>> > varnames;
+
+    stream.seekg(8, std::ios::cur);
+    str< 8 > classname;
+    for (int i = 0; i < num_classes; i++) {
+        stream.read( classname.data(), 8 );
+        classnames.push_back(classname);
+    }
+
+    stream.seekg(8, std::ios::cur);
+    stream.read((char*) vars_in_class.data(), num_classes * 4);
+    std::transform( vars_in_class.begin(),
+                    vars_in_class.end(),
+                    vars_in_class.begin(),
+                    ntoh );
+    auto negative = []( int32_t x ) { return x < 0; };
+    if (std::any_of( vars_in_class.begin(), vars_in_class.end(), negative))
+        throw bad_header("Negative value, corrupted file");
+
+    stream.seekg(8, std::ios::cur);
+    for (int i = 0; i < num_classes; ++i) {
+        varnames[classnames[i]] = {};
+        stream.seekg(4, std::ios::cur); // skip time varname
+        std::vector<char> buf( vars_in_class[i] * 4, 0 );
+        stream.read( buf.data(), vars_in_class[i] * 4 );
+        str<4> varname;
+        for (int k = 0; k < vars_in_class[i]; ++k) {
+            memcpy(varname.data(), buf.data() + k*4, 4);
+            varnames[classnames[i]].push_back(varname);
+        }
+        stream.seekg(8, std::ios::cur);
+    }
+
+    if (stream.eof() || !stream.good())
+        throw unexpected_eof("");
+
+    return varnames;
 }
 
-nex::NexusPlot::NexusPlot( const std::string& filename ) {
+void read_vars(std::istream& stream,
+               std::vector< NexusData >& data,
+               int32_t timestep,
+               float time,
+               int32_t max_perfs,
+               str< 8 > classname,
+               str< 8 > instancename,
+               const std::vector< str< 4 > >& varnames) {
+
+    std::vector< int32_t > values( varnames.size() );
+    stream.read( (char*)values.data(), values.size() * sizeof(int32_t) );
+    std::transform( values.begin(), values.end(), values.begin(), ntoh );
+
+    for (size_t k = 0; k < varnames.size(); k++) {
+        data.push_back( NexusData {
+            timestep,
+            time,
+            max_perfs,
+            classname,
+            instancename,
+            varnames[k],
+            interpret_float( values[k] )
+        });
+    }
+}
+
+}
+
+NexusPlot nex::load( const std::string& filename ) {
     std::ifstream stream(filename, std::ios::binary);
     if ( !stream.good() )
-        throw nex::read_error("Could not open file " + filename);
-    this->load(stream);
+        throw read_error("Could not open file " + filename);
+    return load(stream);
 }
 
-nex::NexusPlot::NexusPlot( std::istream& stream ) {
-    this->load(stream);
-}
-
-void nex::NexusPlot::load(std::istream& stream) {
+NexusPlot nex::load(std::istream& stream) {
     struct stream_guard {
-        stream_guard( std::istream& stream ) :
-            mask( stream.exceptions() ),
-            s( stream ) {}
-        ~stream_guard() { this->s.exceptions( this->mask ); }
+        stream_guard(std::istream &stream) :
+                mask(stream.exceptions()),
+                s(stream) {}
+
+        ~stream_guard() { this->s.exceptions(this->mask); }
+
         std::ios::iostate mask;
-        std::istream& s;
-    } g { stream };
-    stream.exceptions( std::ios::goodbit );
+        std::istream &s;
+    } g{stream};
+    stream.exceptions(std::ios::goodbit);
 
-    stream.seekg(4, std::ios::beg); // skip 4 bytes
-    auto type_header = read_str<10>(stream);
+    auto header = read_header(stream);
+    auto varnames = read_varnames(stream, header.num_classes);
 
-    if (type_header.compare(NEXUS_PLOT_TYPE_HEADER) != 0 || !stream.good())
-        throw nex::bad_header("Could not verify file type");
+    /*
+     * Load data
+     */
 
-    auto header        = headerinfo( stream );
-    this->num_classes   = header.num_classes;
-    this->day           = header.day;
-    this->month         = header.month;
-    this->year          = header.year;
-    this->nx            = header.nx;
-    this->ny            = header.ny;
-    this->nz            = header.nz;
-    this->ncomp         = header.ncomp;
-    this->class_names   = header.class_names;
-    this->vars_in_class = header.vars_in_class;
-    this->var_names     = header.var_names;
+    NexusPlot plt { header, {} };
+    auto& data = plt.data;
+
+    while( true ) {
+        auto classname = read_str<8>(stream);
+
+        static constexpr const char stop[9] = "STOP    ";
+
+        if( std::equal(std::begin(classname), std::end(classname),
+                       std::begin(stop)) )
+            return plt;
+
+        stream.seekg(8, std::ios::cur);
+
+        std::array< int32_t, 5 > buf {};
+        stream.read( (char*)buf.data(), buf.max_size() * 4 );
+        if ( !stream.good() ) throw unexpected_eof("");
+        std::transform( buf.begin(), buf.end(), buf.begin(), ntoh );
+
+        int32_t timestep  = (int32_t) interpret_float(buf[0]);
+        float   time      = interpret_float(buf[1]);
+        int32_t num_items = (int32_t) interpret_float(buf[2]);
+        // int32_t max_items = (int32_t) interpret_float(buf[3]);
+        int32_t max_perfs = (int32_t) interpret_float(buf[4]);
+
+        for (int i = 0; i < num_items; i++) {
+            stream.seekg(8, std::ios::cur);
+            str<8> instancename;
+            stream.read(instancename.data(), 8);
+            stream.seekg(64, std::ios::cur);
+            read_vars(stream,
+                      data,
+                      timestep,
+                      time,
+                      max_perfs,
+                      classname,
+                      instancename,
+                      varnames[classname]);
+        }
+        stream.seekg(8, std::ios::cur);
+    }
 }
 
-ecl_sum_type* nex::NexusPlot::ecl_summary( const std::string& ecl_case ) {
+ecl_sum_type* nex::ecl_summary(const std::string& ecl_case, const NexusPlot& plt) {
+    bool fmt_output = true;
     bool unified = true;
-    bool fmt_output = false;
     const char* key_join_string = ":";
     time_t sim_start = 0;
     bool time_in_days = true;
@@ -164,7 +242,50 @@ ecl_sum_type* nex::NexusPlot::ecl_summary( const std::string& ecl_case ) {
         key_join_string,
         sim_start,
         time_in_days,
-        this->nx, this->ny, this->nz);
+        plt.header.nx, plt.header.ny, plt.header.nz);
+
+    auto data = plt.data;
+
+
+    /*
+
+    1) Lag header
+    2) Lag timesteps
+    3) Set verdier
+
+     */
+
+    auto *smspec = ecl_sum_add_var(ecl_sum, "FOPR", NULL, -1, "x", 0.0);
+
+    auto nex_timesteps = plt.get_unique(get::timestep);
+    auto nex_times = plt.get_unique(get::time);
+    std::vector< ecl_sum_tstep_type* > timesteps;
+    for (size_t i = 0; i < nex_timesteps.size(); i++) {
+        auto* ts = ecl_sum_add_tstep(ecl_sum, nex_timesteps[i], nex_times[i] );
+        timesteps.push_back( ts );
+    }
+
+    /*
+     * Write Field
+     */
+
+    std::vector< NexusData > field;
+    std::copy_if( data.begin(), data.end(), std::back_inserter( field ),
+                  []( const NexusData& nd ) {
+                      return is::classname( "FIELD" )(nd)
+                          && is::instancename( "NETWORK" )(nd);
+                  });
+    std::sort( field.begin(), field.end(), cmp::timestep );
+
+
+    std::vector< NexusData > cop;
+    std::copy_if( field.begin(), field.end(), std::back_inserter( cop ),
+        nex::is::varname( "COP" ));
+
+
+    for (size_t i = 0; i < timesteps.size(); i++) {
+        ecl_sum_tstep_set_from_node( timesteps[i], smspec, cop[i].value );
+    }
 
     return ecl_sum;
 }

agchitu/libecl

lib/nexus/nexus_plot.cpp
@@ -18,9 +18,12 @@
 
 #include <algorithm>
 #include <array>
-#include <sstream>
+#include <cstring>
 #include <iostream>
+#include <map>
 #include <sstream>
+#include <set>
+
 
 #include <ert/util/build_config.h>
 #ifdef HAVE_NETINET_IN_H
@@ -32,128 +35,203 @@
 #endif
 
 #include <ert/nexus/nexus_plot.hpp>
-#include <ert/ecl/ecl_sum.h>
 
+using namespace nex;
 
-const std::string NEXUS_PLOT_TYPE_HEADER = "PLOT  BIN ";
 
 namespace {
 
-template< int N >
-std::string read_str(std::istream& stream) {
-    std::array< char, N> buf;
-    stream.read( buf.data(), N );
-    return std::string( buf.data(), N );
+uint32_t ntoh(uint32_t ui) { return ntohl(ui); }
+
+float interpret_float(int32_t i) {
+    float f;
+    std::memcpy( &f, &i, sizeof( f ) );
+    return f;
 }
 
-struct hinfo {
-    int32_t num_classes;
-    int32_t day, month, year;
-    int32_t nx, ny, nz;
-    int32_t ncomp;
-    std::vector<std::string> class_names;
-    std::vector<int32_t> vars_in_class;
-    std::vector< std::vector<std::string> > var_names;
-};
+template< int N >
+using str = std::array< char, N >;
 
-hinfo headerinfo( std::istream& stream ) {
-    stream.seekg(4 + 10 + 562 + 264, std::ios::beg);
+template< int N >
+str< N > read_str(std::istream& stream) {
+    std::array< char, N> buf;
+    stream.read( buf.data(), N );
+    return buf;
+}
+
+NexusHeader read_header( std::istream& stream ) {
+    stream.seekg(4, std::ios::beg); // skip 4 bytes
+
+    static constexpr const char th0[11] = "PLOT  BIN ";
+    auto th1 = read_str<10>(stream);
+    auto ok = std::equal(std::begin(th1), std::end(th1), std::begin(th0));
+
+    if (!ok || !stream.good())
+        throw bad_header("Could not verify file type");
+
+    // skip unknown blob
+    stream.seekg(562 + 264, std::ios::cur);
 
     std::array< int32_t, 8 > buf {};
     stream.read( (char*)buf.data(), buf.max_size() * 4 );
-    if ( !stream.good() ) throw nex::unexpected_eof("");
+    if ( !stream.good() ) throw unexpected_eof("");
 
-    auto ntoh = []( int32_t x ) { return ntohl( x ); };
     std::transform( buf.begin(), buf.end(), buf.begin(), ntoh );
     auto negative = []( int32_t x ) { return x < 0; };
     if ( std::any_of( buf.begin(), buf.end(), negative ) )
-        throw nex::bad_header("Negative value, corrupted file");
+        throw bad_header("Negative value, corrupted file");
 
-    hinfo h = {
+    NexusHeader h = {
         buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7],
-        {},
-        std::vector< int32_t >( buf[0], 0 ),
-        std::vector< std::vector<std::string> >( buf[0],
-            std::vector<std::string>()),
     };
-
-    stream.seekg(8, std::ios::cur);
-    std::array< char, 8 > class_name;
-    for (int i = 0; i < h.num_classes; i++) {
-        stream.read(class_name.data(), 8);
-        h.class_names.push_back(std::string( class_name.data(), 8 ));
-    }
-
-    stream.seekg(8, std::ios::cur);
-    stream.read((char*) h.vars_in_class.data(), h.num_classes * 4);
-    std::transform( h.vars_in_class.begin(),
-                    h.vars_in_class.end(),
-                    h.vars_in_class.begin(),
-                    ntoh );
-    if (std::any_of( h.vars_in_class.begin(), h.vars_in_class.end(), negative))
-        throw nex::bad_header("Negative value, corrupted file");
-
-    stream.seekg(8, std::ios::cur);
-    for (int i = 0; i < h.num_classes; ++i) {
-        stream.seekg(4, std::ios::cur);
-        std::vector< char > var_names( h.vars_in_class[i] * 4, 0 );
-        stream.read( var_names.data(), h.vars_in_class[i] * 4 );
-        for (int k = 0; k < h.vars_in_class[i]; ++k)
-            h.var_names[i].push_back( std::string( var_names.data() + k*4 ,4 ));
-        stream.seekg(8, std::ios::cur);
-    }
-    stream.seekg(4, std::ios::cur);
 
     return h;
 }
 
+std::map< str<8>, std::vector<str<4>> >
+read_varnames( std::istream& stream, int32_t num_classes )  {
+    std::vector< str<8> > classnames;
+    std::vector< int32_t > vars_in_class( num_classes );
+    std::map< str<8>, std::vector<str<4>> > varnames;
+
+    stream.seekg(8, std::ios::cur);
+    str< 8 > classname;
+    for (int i = 0; i < num_classes; i++) {
+        stream.read( classname.data(), 8 );
+        classnames.push_back(classname);
+    }
+
+    stream.seekg(8, std::ios::cur);
+    stream.read((char*) vars_in_class.data(), num_classes * 4);
+    std::transform( vars_in_class.begin(),
+                    vars_in_class.end(),
+                    vars_in_class.begin(),
+                    ntoh );
+    auto negative = []( int32_t x ) { return x < 0; };
+    if (std::any_of( vars_in_class.begin(), vars_in_class.end(), negative))
+        throw bad_header("Negative value, corrupted file");
+
+    stream.seekg(8, std::ios::cur);
+    for (int i = 0; i < num_classes; ++i) {
+        varnames[classnames[i]] = {};
+        stream.seekg(4, std::ios::cur); // skip time varname
+        std::vector<char> buf( vars_in_class[i] * 4, 0 );
+        stream.read( buf.data(), vars_in_class[i] * 4 );
+        str<4> varname;
+        for (int k = 0; k < vars_in_class[i]; ++k) {
+            memcpy(varname.data(), buf.data() + k*4, 4);
+            varnames[classnames[i]].push_back(varname);
+        }
+        stream.seekg(8, std::ios::cur);
+    }
+
+    if (stream.eof() || !stream.good())
+        throw unexpected_eof("");
+
+    return varnames;
 }
 
-nex::NexusPlot::NexusPlot( const std::string& filename ) {
+void read_vars(std::istream& stream,
+               std::vector< NexusData >& data,
+               int32_t timestep,
+               float time,
+               int32_t max_perfs,
+               str< 8 > classname,
+               str< 8 > instancename,
+               const std::vector< str< 4 > >& varnames) {
+
+    std::vector< int32_t > values( varnames.size() );
+    stream.read( (char*)values.data(), values.size() * sizeof(int32_t) );
+    std::transform( values.begin(), values.end(), values.begin(), ntoh );
+
+    for (size_t k = 0; k < varnames.size(); k++) {
+        data.push_back( NexusData {
+            timestep,
+            time,
+            max_perfs,
+            classname,
+            instancename,
+            varnames[k],
+            interpret_float( values[k] )
+        });
+    }
+}
+
+}
+
+NexusPlot nex::load( const std::string& filename ) {
     std::ifstream stream(filename, std::ios::binary);
     if ( !stream.good() )
-        throw nex::read_error("Could not open file " + filename);
-    this->load(stream);
+        throw read_error("Could not open file " + filename);
+    return load(stream);
 }
 
-nex::NexusPlot::NexusPlot( std::istream& stream ) {
-    this->load(stream);
-}
-
-void nex::NexusPlot::load(std::istream& stream) {
+NexusPlot nex::load(std::istream& stream) {
     struct stream_guard {
-        stream_guard( std::istream& stream ) :
-            mask( stream.exceptions() ),
-            s( stream ) {}
-        ~stream_guard() { this->s.exceptions( this->mask ); }
+        stream_guard(std::istream &stream) :
+                mask(stream.exceptions()),
+                s(stream) {}
+
+        ~stream_guard() { this->s.exceptions(this->mask); }
+
         std::ios::iostate mask;
-        std::istream& s;
-    } g { stream };
-    stream.exceptions( std::ios::goodbit );
+        std::istream &s;
+    } g{stream};
+    stream.exceptions(std::ios::goodbit);
 
-    stream.seekg(4, std::ios::beg); // skip 4 bytes
-    auto type_header = read_str<10>(stream);
+    auto header = read_header(stream);
+    auto varnames = read_varnames(stream, header.num_classes);
 
-    if (type_header.compare(NEXUS_PLOT_TYPE_HEADER) != 0 || !stream.good())
-        throw nex::bad_header("Could not verify file type");
+    /*
+     * Load data
+     */
 
-    auto header        = headerinfo( stream );
-    this->num_classes   = header.num_classes;
-    this->day           = header.day;
-    this->month         = header.month;
-    this->year          = header.year;
-    this->nx            = header.nx;
-    this->ny            = header.ny;
-    this->nz            = header.nz;
-    this->ncomp         = header.ncomp;
-    this->class_names   = header.class_names;
-    this->vars_in_class = header.vars_in_class;
-    this->var_names     = header.var_names;
+    NexusPlot plt { header, {} };
+    auto& data = plt.data;
+
+    while( true ) {
+        auto classname = read_str<8>(stream);
+
+        static constexpr const char stop[9] = "STOP    ";
+
+        if( std::equal(std::begin(classname), std::end(classname),
+                       std::begin(stop)) )
+            return plt;
+
+        stream.seekg(8, std::ios::cur);
+
+        std::array< int32_t, 5 > buf {};
+        stream.read( (char*)buf.data(), buf.max_size() * 4 );
+        if ( !stream.good() ) throw unexpected_eof("");
+        std::transform( buf.begin(), buf.end(), buf.begin(), ntoh );
+
+        int32_t timestep  = (int32_t) interpret_float(buf[0]);
+        float   time      = interpret_float(buf[1]);
+        int32_t num_items = (int32_t) interpret_float(buf[2]);
+        // int32_t max_items = (int32_t) interpret_float(buf[3]);
+        int32_t max_perfs = (int32_t) interpret_float(buf[4]);
+
+        for (int i = 0; i < num_items; i++) {
+            stream.seekg(8, std::ios::cur);
+            str<8> instancename;
+            stream.read(instancename.data(), 8);
+            stream.seekg(64, std::ios::cur);
+            read_vars(stream,
+                      data,
+                      timestep,
+                      time,
+                      max_perfs,
+                      classname,
+                      instancename,
+                      varnames[classname]);
+        }
+        stream.seekg(8, std::ios::cur);
+    }
 }
 
-ecl_sum_type* nex::NexusPlot::ecl_summary( const std::string& ecl_case ) {
+ecl_sum_type* nex::ecl_summary(const std::string& ecl_case, const NexusPlot& plt) {
+    bool fmt_output = true;
     bool unified = true;
-    bool fmt_output = false;
     const char* key_join_string = ":";
     time_t sim_start = 0;
     bool time_in_days = true;
@@ -164,7 +242,50 @@ ecl_sum_type* nex::NexusPlot::ecl_summary( const std::string& ecl_case ) {
         key_join_string,
         sim_start,
         time_in_days,
-        this->nx, this->ny, this->nz);
+        plt.header.nx, plt.header.ny, plt.header.nz);
+
+    auto data = plt.data;
+
+
+    /*
+
+    1) Lag header
+    2) Lag timesteps
+    3) Set verdier
+
+     */
+
+    auto *smspec = ecl_sum_add_var(ecl_sum, "FOPR", NULL, -1, "x", 0.0);
+
+    auto nex_timesteps = plt.get_unique(get::timestep);
+    auto nex_times = plt.get_unique(get::time);
+    std::vector< ecl_sum_tstep_type* > timesteps;
+    for (size_t i = 0; i < nex_timesteps.size(); i++) {
+        auto* ts = ecl_sum_add_tstep(ecl_sum, nex_timesteps[i], nex_times[i] );
+        timesteps.push_back( ts );
+    }
+
+    /*
+     * Write Field
+     */
+
+    std::vector< NexusData > field;
+    std::copy_if( data.begin(), data.end(), std::back_inserter( field ),
+                  []( const NexusData& nd ) {
+                      return is::classname( "FIELD" )(nd)
+                          && is::instancename( "NETWORK" )(nd);
+                  });
+    std::sort( field.begin(), field.end(), cmp::timestep );
+
+
+    std::vector< NexusData > cop;
+    std::copy_if( field.begin(), field.end(), std::back_inserter( cop ),
+        nex::is::varname( "COP" ));
+
+
+    for (size_t i = 0; i < timesteps.size(); i++) {
+        ecl_sum_tstep_set_from_node( timesteps[i], smspec, cop[i].value );
+    }
 
     return ecl_sum;
 }